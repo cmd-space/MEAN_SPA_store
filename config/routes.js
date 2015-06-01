c83152c2726382782dc1a17cdb1fa5ce8e5fb418
mec64 <masonecrane@gmail.com>
2015-06-01 14:46:04 -0600

    working on layout for all the partials, and moving on to db work and controllers

---
+++ config/routes.js
@@ -0,0 +1,8 @@
+//var customers = require('./../server/controllers/customers.js');
+//var orders = require('./../server/controllers/orders.js');
+
+module.exports = function(app){
+//    app.get('/', function(req, res){
+//        
+//    });
+}
